27c246ce5bb6d7a6ce0d53a50bee4ebc98c16389
Redian Fikri <redianmf@gmail.com>
2024-04-23 20:14:47 +0700

    fix(schema): change user schema

--- database/sql_migrations/1_user_schema.sql
+++ database/sql_migrations/1_user_schema.sql
@@ -4,8 +4,8 @@
 CREATE TABLE users (
     id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(50),
-    email VARCHAR(30),
-    password VARCHAR(50),
+    email VARCHAR(50),
+    password VARCHAR(255),
     birth_date DATE,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
